4b852b694e3fe40b25f3e408fdd24257f722d982
Mario Vega <malbertovega@gmail.com>
2022-08-31 23:42:35 -0500

    a fixed number of decimals stablished

--- E8.2-windchill.py
+++ E8.2-windchill.py
@@ -12,7 +12,7 @@ def main():
         else:
             for j in range(-20, 70, 10):
                 windchillIndex = 35.74 + (0.6215 * j) - (35.75 * (i ** 0.16)) + (0.4275 * j * (i ** 0.16))
-                wI.append("{0:0.3}".format(windchillIndex))
+                wI.append("{0:0.3f}".format(windchillIndex))
             printTable(wI[0], wI[1], wI[2], wI[3], wI[4], wI[5], wI[6], wI[7], wI[8], wI[9])
 
 main()
