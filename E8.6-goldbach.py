05c4225aca48d65ded5ddd9eb2aabc76aa3fa5e4
Mario Vega <malbertovega@gmail.com>
2022-09-05 21:54:53 -0500

    is_prime function modified

--- E8.6-goldbach.py
+++ E8.6-goldbach.py
@@ -2,14 +2,12 @@ import math
 
 def is_prime(n):
     d2 = math.sqrt(n) // 1       # upper limit of divisor
-    x = 1                        # prime checker: 1 --> yes, 0 --> no
     while d2 >= 2:
         isPrime = n / d2
         if isPrime.is_integer():
-            x = 0
-            break
+            return False
         d2 = d2 - 1
-    return x
+    return True
 
 def main():
     while True:
@@ -19,9 +17,9 @@ def main():
     print('Possible combinations of prime numbers that equal {0} when added:\n'.format(n))
     
     for i in range(int(n/2), 0, -1):
-        if is_prime(i) == 1:            # is prime
+        if is_prime(i):
             for j in range(int(n/2), n):
-                if is_prime(j) == 1 and i + j == n:    # is prime
+                if is_prime(j) and i + j == n:
                     print ('{0} + {1} = {2}'.format(i, j, n))
 
 main()
