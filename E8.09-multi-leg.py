0ea948cbf36b8392a809928d0c3d643411b283e8
Mario Vega <malbertovega@gmail.com>
2022-09-07 12:33:28 -0500

    fixed decimal places set

--- E8.09-multi-leg.py
+++ E8.09-multi-leg.py
@@ -14,14 +14,14 @@ def main():
                 odo2, gas = float(leg[0]), float(leg[1])
                 miles_per_leg = odo2 - odo1
                 mpg_per_leg = miles_per_leg / gas
-                print('MPG achieved in this leg: {0}'.format(mpg_per_leg))
+                print('MPG achieved in this leg: {0:0.2f}'.format(mpg_per_leg))
                 odo1 = odo2
                 route = route + miles_per_leg
                 total_gas = total_gas + gas
             else:
                 odo2 = leg
         
-        print('The total MPG for the trip is: {0}'.format(route/total_gas))
+        print('The total MPG for the trip is: {0:0.2f}'.format(route/total_gas))
 
     except:
         print('Enter data according instructions')
